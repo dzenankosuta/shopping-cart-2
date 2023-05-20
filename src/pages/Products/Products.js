64dec6986170c07926101270e81d9336fabc1402
Dzenan Košuta <dzenan_kosuta@hotmail.com>
2023-05-20 18:38:41 +0200

    adding product card, new styles for navbar, footer

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,5 +1,10 @@
 import React from "react";
+import ProductCard from "../../components/Card/ProductCard";
 
 export default function Products() {
-  return <div>Products</div>;
+  return (
+    <>
+      <ProductCard />
+    </>
+  );
 }
