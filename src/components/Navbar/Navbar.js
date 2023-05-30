67da2409d245d46511dc79ab964a664493ef9b7f
Dzenan Košuta <dzenan_kosuta@hotmail.com>
2023-05-30 21:06:07 +0200

    adding num of added products to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Navbar.css";
 import { NavLink } from "react-router-dom";
 // import logo from "../../assets/images/logo.png";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { AppContext } from "../../context/AppContext";
 
 export default function Navbar() {
+  const { cart } = useContext(AppContext);
+
   return (
     <header className="navbar">
       <NavLink to={"/"}>
@@ -15,6 +18,14 @@ export default function Navbar() {
         />
       </NavLink>
       <div className="links">
+        <NavLink
+          to={"/sale"}
+          className={({ isActive }) =>
+            isActive ? "activeStyles sale" : "classicStyles sale"
+          }
+        >
+          <h2>ON SALE</h2>
+        </NavLink>
         <NavLink
           to={"/products"}
           className={({ isActive }) =>
@@ -29,7 +40,26 @@ export default function Navbar() {
             isActive ? "activeStyles" : "classicStyles"
           }
         >
-          <ShoppingCartIcon className="cartIcon" fontSize="large" />
+          {cart.length ? (
+            <div className="cart-icon-container">
+              <div className="cart-icon">
+                <p style={{ color: "#fff" }}>{cart.length}</p>
+              </div>
+              <ShoppingCartIcon
+                className="cartIcon"
+                fontSize="large"
+                style={{ width: "100%", height: "100%" }}
+              />
+            </div>
+          ) : (
+            <div className="cart-icon-container">
+              <ShoppingCartIcon
+                className="cartIcon"
+                fontSize="large"
+                style={{ width: "100%", height: "100%" }}
+              />
+            </div>
+          )}
         </NavLink>
       </div>
     </header>
